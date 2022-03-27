ffc452e0e9ee6cfbf7927bdcbac5977a471230a7
KHTangent <contact@khtangent.com>
2022-03-27 17:27:24 +0200

    Increment version number

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -8,7 +8,7 @@ let newCommand: Command = {
 				"By KHTangent\n" +
 				"Licensed under the MIT License.\n" +
 				"<https://github.com/KHTangent/FjordFursBot>\n" +
-				`Version 0.6.2`
+				`Version 0.7.0`
 		);
 	},
 };
